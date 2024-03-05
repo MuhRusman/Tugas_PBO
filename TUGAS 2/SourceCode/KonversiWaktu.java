e126ac13e3fa5277848333eea4069c5035fcfbba
MuhRusman <161143721+MuhRusman@users.noreply.github.com>
2024-03-05 17:50:47 +1300

    Add files via upload

---
+++ TUGAS 2/SourceCode/KonversiWaktu.java
@@ -0,0 +1,19 @@
+// 13020220068, Muh  Rusman
+//Konversi Waktu
+
+
+import java.util.Scanner;
+public class KonversiWaktu {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan Total Detik : ");
+        long totalDetik = input.nextLong();
+        long detikSekarang = totalDetik % 60;
+        long totalMenit = totalDetik / 60;
+        long menitSekarang = totalMenit % 60;
+        long totalJam = totalMenit / 60;
+        long jamSekarang = totalJam % 24;
+
+        System.out.println("Tampil Waktu : "+ jamSekarang+ ":"+ menitSekarang +":"+ detikSekarang);
+    }
+}
